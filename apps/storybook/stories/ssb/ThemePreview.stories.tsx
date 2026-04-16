5ac5d79b011cb30094469edbaa2e6830e2bac9b2
Carina <cgn@ssb.no>
2026-04-16 14:48:31 +0200

    Added ToogleGroup color

--- apps/storybook/stories/ssb/ThemePreview.stories.tsx
+++ apps/storybook/stories/ssb/ThemePreview.stories.tsx
@@ -4,6 +4,7 @@ import { ThemePreview } from './ThemePreview'
 import { Fieldset, ToggleGroup } from '@digdir/designsystemet-react'
 import { SunIcon, MoonIcon } from '@navikt/aksel-icons'
 import classes from './ThemePreview.module.css'
+//import type { Color } from '../../../packages/theme/brand/types.d.ts'
 
 const meta = {
   title: 'SSB/Theme Preview',
@@ -18,15 +19,17 @@ export default meta
 type Story = StoryObj<typeof meta>
 
 const sizes = ['sm', 'md', 'lg']
-const colorModes = ['light', 'dark']
+const colorSchemeModes = ['light', 'dark']
+const colorModes = ['accent', 'neutral', 'brand1', 'brand2']
 
 export const Default: StoryFn = () => {
   const [size, setSize] = useState('sm')
-  const [colorMode, setColorMode] = useState('light')
+  const [colorSchemeMode, setColorSchemeMode] = useState('light')
+  const [colorMode, setColorMode] = useState('accent')
 
   return (
-    <div data-color-scheme={colorMode} className={classes.wrapper}>
-      <div data-color-scheme={colorMode} className={classes.innerWrapper}>
+    <div data-color-scheme={colorSchemeMode} className={classes.wrapper}>
+      <div data-color-scheme={colorSchemeMode} className={classes.innerWrapper}>
         <div className={classes.controls} data-size='sm'>
           <Fieldset style={{ display: 'grid', gap: '1rem' }}>
             <Fieldset.Legend>
@@ -47,8 +50,8 @@ export const Default: StoryFn = () => {
               Fargemodus
               <code data-size='xs'>(data-color-scheme)</code>
             </Fieldset.Legend>
-            <ToggleGroup variant='secondary' value={colorMode} onChange={setColorMode}>
-              {colorModes.map((color) => (
+            <ToggleGroup variant='secondary' value={colorSchemeMode} onChange={setColorSchemeMode}>
+              {colorSchemeModes.map((color) => (
                 <ToggleGroup.Item key={color} value={color}>
                   {color === 'light' ? <SunIcon aria-hidden /> : <MoonIcon aria-hidden />}
                   {color}
@@ -56,8 +59,21 @@ export const Default: StoryFn = () => {
               ))}
             </ToggleGroup>
           </Fieldset>
+          <Fieldset>
+            <Fieldset.Legend>
+              Fargetema
+              <code data-size='xs'>(data-color)</code>
+            </Fieldset.Legend>
+            <ToggleGroup variant='secondary' value={colorMode} onChange={setColorMode}>
+              {colorModes.map((color) => (
+                <ToggleGroup.Item key={color} value={color}>
+                  {color}
+                </ToggleGroup.Item>
+              ))}
+            </ToggleGroup>
+          </Fieldset>
         </div>
-        <ThemePreview data-size={size} data-color-scheme={colorMode} />
+        <ThemePreview data-size={size} data-color={colorMode} data-color-scheme={colorSchemeMode} />
       </div>
     </div>
   )
