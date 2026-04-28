c8ebec21266c083f0f8ec9e6246609c8e43ffccd
Carina <cgn@ssb.no>
2026-04-28 14:48:29 +0200

    endret til accent

--- packages/theme/brand/colors.d.ts
+++ packages/theme/brand/colors.d.ts
@@ -5,7 +5,7 @@ import type {} from '@digdir/designsystemet-types';
 // Augment types based on theme
 declare module '@digdir/designsystemet-types' {
   export interface ColorDefinitions {
-    primary: never;
+    accent: never;
     coolgray: never;
     magic: never;
     neutral: never;
