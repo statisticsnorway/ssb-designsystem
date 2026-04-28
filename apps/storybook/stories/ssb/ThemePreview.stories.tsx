3b9c69246d0e37f243d6f40e088d58e53f7ea9cd
Carina <cgn@ssb.no>
2026-04-28 15:41:40 +0200

    liten oppdatering

--- apps/storybook/stories/ssb/ThemePreview.stories.tsx
+++ apps/storybook/stories/ssb/ThemePreview.stories.tsx
@@ -29,7 +29,7 @@ type Story = StoryObj<typeof meta>
 
 const sizes = ['sm', 'md', 'lg']
 const colorSchemeModes = ['light', 'dark']
-const colorModes = ['accent', 'neutral', 'inverted']
+const colorModes = ['accent', 'neutral', 'brand1', 'brand2']
 
 export const Default: StoryFn = () => {
   const [size, setSize] = useState('sm')
