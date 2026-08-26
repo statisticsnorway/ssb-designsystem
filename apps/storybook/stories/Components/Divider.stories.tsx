d96b6be17f5e432de76e96c0039b2268bbd07e82
Carina <cgn@ssb.no>
2026-08-26 09:58:49 +0200

    Improved story for component Divider

--- apps/storybook/stories/Components/Divider.stories.tsx
+++ apps/storybook/stories/Components/Divider.stories.tsx
@@ -1,22 +1,9 @@
 import type { Meta, StoryObj } from '@storybook/react-vite'
 import { Divider, Paragraph } from '@statisticsnorway/design-react'
 
-const description = `
-Divider brukes for å skape et visuelt skille mellom innhold.
-
-Se full dokumentasjon: https://designsystemet.no/no/components/docs/divider/overview
-`
-
 const meta: Meta<typeof Divider> = {
   title: 'Komponenter/Divider',
   component: Divider,
-  parameters: {
-    docs: {
-      description: {
-        component: description,
-      },
-    },
-  },
 }
 
 export default meta
@@ -25,12 +12,10 @@ type Story = StoryObj<typeof Divider>
 
 export const Default: Story = {
   render: () => (
-    <div style={{ maxWidth: '400px' }}>
-      <Paragraph>Dette er innhold over en skillelinje.</Paragraph>
-
+    <>
+      <Paragraph>Divider er brukt for å dele opp innhold i mindre deler.</Paragraph>
       <Divider />
-
-      <Paragraph>Dette er innhold under skillelinjen.</Paragraph>
-    </div>
+      <Paragraph>Den kan også brukes for å skille innhold som er relatert til hverandre.</Paragraph>
+    </>
   ),
 }
