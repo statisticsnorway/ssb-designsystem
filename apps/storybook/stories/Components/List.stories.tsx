8457c281650ae13b0ee142f1e1c67da596169c32
Carina <cgn@ssb.no>
2026-09-07 10:11:02 +0200

    Improved doc list component

--- apps/storybook/stories/Components/List.stories.tsx
+++ apps/storybook/stories/Components/List.stories.tsx
@@ -1,23 +1,9 @@
 import type { Meta, StoryObj } from '@storybook/react-vite'
 import { Heading, List } from '@statisticsnorway/design-react'
 
-const description = `
-List brukes for å presentere innhold på en oversiktlig og strukturert måtes.
-
-Se full dokumentasjon:
-https://designsystemet.no/no/components/docs/list/overview
-`
-
 const meta: Meta<typeof List.Unordered> = {
   title: 'Komponenter/List',
   component: List.Unordered,
-  parameters: {
-    docs: {
-      description: {
-        component: description,
-      },
-    },
-  },
 }
 
 export default meta
@@ -26,37 +12,24 @@ type Story = StoryObj<typeof meta>
 export const Default: Story = {
   render: () => (
     <List.Unordered>
-      <List.Item>Brød</List.Item>
-      <List.Item>Melk</List.Item>
-      <List.Item>Egg</List.Item>
+      <List.Item>Bøyabreen</List.Item>
+      <List.Item>Briksdalsbreen</List.Item>
+      <List.Item>Nigardsbreen</List.Item>
     </List.Unordered>
   ),
 }
 
-export const Variants: Story = {
+export const Ordered: Story = {
   render: () => (
-    <div style={{ display: 'grid', gap: '2rem' }}>
-      <div>
-        <Heading level={2} data-size='xs' style={{ marginBottom: '0.5rem' }}>
-          Usortert liste
-        </Heading>
-        <List.Unordered>
-          <List.Item>Brød</List.Item>
-          <List.Item>Melk</List.Item>
-          <List.Item>Egg</List.Item>
-        </List.Unordered>
-      </div>
-
-      <div>
-        <Heading level={2} data-size='xs' style={{ marginBottom: '0.5rem' }}>
-          Sortert liste
-        </Heading>
-        <List.Ordered>
-          <List.Item>Forvarm ovnen</List.Item>
-          <List.Item>Sett inn formen</List.Item>
-          <List.Item>Stek i 20 minutter</List.Item>
-        </List.Ordered>
-      </div>
-    </div>
+    <>
+      <Heading level={2} data-size='xs' style={{ marginBottom: '0.5rem' }}>
+        Sortert liste
+      </Heading>
+      <List.Ordered>
+        <List.Item>Forvarm ovnen</List.Item>
+        <List.Item>Sett inn formen</List.Item>
+        <List.Item>Stek i 20 minutter</List.Item>
+      </List.Ordered>
+    </>
   ),
 }
