f9872bee315c842f69e0c75648649ca337cbde28
Carina <cgn@ssb.no>
2026-09-08 10:33:21 +0200

    Improved documentation Select

--- apps/storybook/stories/Components/Select.stories.tsx
+++ apps/storybook/stories/Components/Select.stories.tsx
@@ -1,23 +1,9 @@
 import type { Meta, StoryObj } from '@storybook/react-vite'
-import { Field, Label, Select } from '@statisticsnorway/design-react'
-
-const description = `
-Select lar brukeren velge ett alternativ fra en liste.
-
-Se full dokumentasjon:
-https://designsystemet.no/no/components/docs/select/overview
-`
+import { Field, Divider, Label, Select } from '@statisticsnorway/design-react'
 
 const meta: Meta<typeof Select> = {
   title: 'Komponenter/Select',
   component: Select,
-  parameters: {
-    docs: {
-      description: {
-        component: description,
-      },
-    },
-  },
 }
 
 export default meta
@@ -29,7 +15,7 @@ export const Default: Story = {
       <Label>Velg et fjell</Label>
       <Select defaultValue=''>
         <Select.Option value='' disabled>
-          Velg et fjell …
+          Velg et fjell &hellip;
         </Select.Option>
         <Select.Option value='everest'>Mount Everest</Select.Option>
         <Select.Option value='aconcagua'>Aconcagua</Select.Option>
@@ -44,38 +30,36 @@ export const Default: Story = {
   ),
 }
 
-export const Variants: Story = {
+export const Group: Story = {
   render: () => (
-    <div style={{ display: 'grid', gap: '2rem', maxWidth: '28rem' }}>
-      <div>
-        <Label style={{ display: 'block', marginBottom: '0.5rem' }}>Med gruppering</Label>
-        <Field>
-          <Select>
-            <Select.Optgroup label='Grünerløkka'>
-              <Select.Option value='sofienbergparken'>Sofienbergparken</Select.Option>
-              <Select.Option value='birkelunden'>Birkelunden</Select.Option>
-              <Select.Option value='olafryesplass'>Olaf Ryes plass</Select.Option>
-            </Select.Optgroup>
-            <Select.Optgroup label='Sentrum'>
-              <Select.Option value='slottsparken'>Slottsparken</Select.Option>
-              <Select.Option value='studenterlunden'>Studenterlunden</Select.Option>
-            </Select.Optgroup>
-            <Select.Optgroup label='Gamle Oslo'>
-              <Select.Option value='botsparken'>Botsparken</Select.Option>
-              <Select.Option value='klosterenga'>Klosterenga park</Select.Option>
-            </Select.Optgroup>
-          </Select>
-        </Field>
-      </div>
+    <Field>
+      <Label>Velg en park</Label>
+      <Select>
+        <Select.Optgroup label='Grünerløkka'>
+          <Select.Option value='sofienbergparken'>Sofienbergparken</Select.Option>
+          <Select.Option value='birkelunden'>Birkelunden</Select.Option>
+          <Select.Option value='olafryesplass'>Olaf Ryes plass</Select.Option>
+        </Select.Optgroup>
+        <Select.Optgroup label='Sentrum'>
+          <Select.Option value='slottsparken'>Slottsparken</Select.Option>
+          <Select.Option value='studenterlunden'>Studenterlunden</Select.Option>
+        </Select.Optgroup>
+        <Select.Optgroup label='Gamle Oslo'>
+          <Select.Option value='botsparken'>Botsparken</Select.Option>
+          <Select.Option value='klosterenga'>Klosterenga park</Select.Option>
+        </Select.Optgroup>
+      </Select>
+    </Field>
+  ),
+}
 
-      <div>
-        <Field>
-          <Label>Kun lesbar</Label>
-          <Select aria-readonly value='everest'>
-            <Select.Option value='everest'>Mount Everest</Select.Option>
-          </Select>
-        </Field>
-      </div>
-    </div>
+export const ReadOnly: Story = {
+  render: () => (
+    <Field>
+      <Label>Velg et fjell</Label>
+      <Select aria-readonly value='everest'>
+        <Select.Option value='everest'>Mount Everest</Select.Option>
+      </Select>
+    </Field>
   ),
 }
